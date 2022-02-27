dffe0055f14ebd81ff62a4d541f51a6d14fb2d86
KYQPaes <kyq.paes@gmail.com>
2022-02-27 00:14:45 -0300

    KRP - Aplicação LES

---
+++ src/test/java/WalletHotline/les/LesApplicationTests.java
@@ -0,0 +1,13 @@
+package WalletHotline.les;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
