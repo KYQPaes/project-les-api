c4b16cdec7d9fc179bd5058e9f8eda9567017db2
KYQPaes <kyq.paes@gmail.com>
2022-03-06 13:08:43 -0300

    KYQ - Correção CORS

--- src/main/java/WalletHotline/les/Controller/ClienteController.java
+++ src/main/java/WalletHotline/les/Controller/ClienteController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(maxAge = 9999)
+@CrossOrigin(maxAge = 9999) ////////////////// problemas com cors
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
