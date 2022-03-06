b78649809ee81c74082347d338f7415510672887
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-03-06 16:06:29 -0300

    endereco

--- src/main/java/WalletHotline/les/Controller/EnderecoController.java
+++ src/main/java/WalletHotline/les/Controller/EnderecoController.java
@@ -1,5 +1,6 @@
 package WalletHotline.les.Controller;
 
+import WalletHotline.les.Model.ClienteModel;
 import WalletHotline.les.Model.EnderecoModel;
 import WalletHotline.les.Repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,11 @@ public class EnderecoController {
     @Autowired
     private EnderecoRepository repository;
 
-    // read
-    // @GetMapping(path = "/api/enderecos/{id}")
-    // @CrossOrigin(origins = "http://192.168.1.19:8080")
-    // public ResponseEntity consultar(@PathVariable("id") Integer id) {
-    // return repository.findById(id).map(record ->
-    // ResponseEntity.ok().body(record)).orElseThrow();
-    // }
+    @GetMapping(path = "/api/enderecos/{id}")
+    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    public ResponseEntity consultar(@PathVariable("id") Integer id) {
+        return repository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElseThrow();
+    }
 
     @GetMapping(path = "/api/enderecos/clienteid/{id}")
     @CrossOrigin(origins = "http://192.168.1.19:8080")
@@ -26,24 +25,31 @@ public class EnderecoController {
         return repository.findByClienteId(cliente_id);
     }
 
+    // update
+    @PutMapping(path = "/api/enderecos") // salva e atualiza caso tenha um id
+    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    public EnderecoModel atualizar(@RequestBody EnderecoModel endereco) {
+        return repository.save(endereco);
+    }
+
     // create/update
-    // @PostMapping(path = "/api/enderecos") // salva e atualiza caso tenha um id
-    // @CrossOrigin(origins = "http://192.168.1.19:8080")
-    // public EnderecoModel salvar(@RequestBody EnderecoModel endereco) {
-    // return repository.save(endereco);
-    // }
+    @PostMapping(path = "/api/enderecos") // salva e atualiza caso tenha um id
+    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    public EnderecoModel salvar(@RequestBody EnderecoModel endereco) {
+        return repository.save(endereco);
+    }
 
     // get
-    // @GetMapping(path = "/api/enderecos")
-    // @CrossOrigin(origins = "http://192.168.1.19:8080")
-    // public Iterable<EnderecoModel> consultarTodos() {
-    // return repository.findAll();
-    // }
+    @GetMapping(path = "/api/enderecos")
+    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    public Iterable<EnderecoModel> consultarTodos() {
+        return repository.findAll();
+    }
 
     // delete
-    // @DeleteMapping(path = "/api/enderecos/{id}")
-    // @CrossOrigin(origins = "http://192.168.1.19:8080")
-    // public void deletar(@PathVariable("id") Integer id) {
-    // repository.deleteById(id);
-    // }
+    @DeleteMapping(path = "/api/enderecos/{id}")
+    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    public void deletar(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+    }
 }
