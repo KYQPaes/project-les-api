4265e371558064dd0e0bdbdc0536da0c13d71523
KYQPaes <kyq.paes@gmail.com>
2022-03-06 13:09:00 -0300

    Mudanças Cartao

--- src/main/java/WalletHotline/les/Controller/CartaoController.java
+++ src/main/java/WalletHotline/les/Controller/CartaoController.java
@@ -6,15 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(maxAge = 9999)
 @RestController
+@RequestMapping("/api/cartoes")
 public class CartaoController {
 
         @Autowired
         private CartaoRepository repository;
 
-        @GetMapping(path = "/api/cartoes/clienteid/{id}")
+        @GetMapping(path = "/clienteid/{id}")
         @CrossOrigin(origins = "http://192.168.1.19:8080")
         public Iterable<CartaoModel> cliente_id(@PathVariable("id") Integer cliente_id) {
                 return repository.findByClienteId(cliente_id);
         }
+
+        // @GetMapping(path = "/api/cartoes/{id}")
+        // @CrossOrigin(origins = "http://192.168.1.19:8080")
+        // public ResponseEntity consultar(@PathVariable("id") Integer id) {
+        // return repository.findById(id).map(record ->
+        // ResponseEntity.ok().body(record)).orElseThrow();
+        // }
+
+        // @GetMapping(path = "/api/cartoes")
+        // @CrossOrigin(origins = "http://192.168.1.19:8080")
+        // public Iterable<CartaoModel> listar() {
+        // return repository.findAll();
+        // }
 }
