a5409856677b2f479e650ac6adc63059b2f05ae1
KYQPaes <kyq.paes@gmail.com>
2022-03-06 03:23:54 -0300

    KRP - As mudanças realizadas no Cliente podem resolver os problemas de CORS

--- src/main/java/WalletHotline/les/Controller/ClienteController.java
+++ src/main/java/WalletHotline/les/Controller/ClienteController.java
@@ -6,49 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(maxAge = 9999)
 @RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository repository;
 
     // read teste
-    @GetMapping(path = "/api/clientes/{id}")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @GetMapping(path = "/{id}")
     public ResponseEntity consultar(@PathVariable("id") Integer id) {
         return repository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElseThrow();
     }
 
-    @GetMapping(path = "/api/clientes/log")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @GetMapping(path = "/log")
     public Iterable<ClienteModel> consultarLogin(@RequestParam String email, @RequestParam String senha) {
         return repository.findByEmailAndSenha(email, senha);
     }
 
     // create
-    @PostMapping(path = "/api/clientes")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @PostMapping(path = "")
     public ClienteModel salvar(@RequestBody ClienteModel cliente) {
         return repository.save(cliente);
     }
 
     // update
-    @PutMapping(path = "/api/clientes")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @PutMapping(path = "")
     public ClienteModel atualizar(@RequestBody ClienteModel cliente) {
         return repository.save(cliente);
     }
 
     // get
-    @GetMapping(path = "/api/clientes")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @GetMapping(path = "")
     public Iterable<ClienteModel> consultarTodos() {
         return repository.findAll();
     }
 
     // delete
-    @DeleteMapping(path = "/api/clientes/{id}")
-    @CrossOrigin(origins = "http://192.168.1.19:8080")
+    @DeleteMapping(path = "/{id}")
     public void deletar(@PathVariable("id") Integer id) {
         repository.deleteById(id);
     }
